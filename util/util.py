fe94913623e3b23e8c120fab95cb54a74dbd0d95
rraisaar <rraisaar@asu.edu>
2023-04-25 10:18:49 -0700

    Secret sharing for image

--- util/util.py
+++ util/util.py
@@ -1,17 +1,11 @@
-def stringToInteger(messageString):
-    integerValue = ''.join(map(str,[ord(char) for char in messageString.lower()]))
-    return int(integerValue)
-    
-def integerToString(messageInteger):
-    st = 0
-    messageIntegerStr = str(messageInteger)
-    stringArray = []
-    while st < len(messageIntegerStr):
-        num = messageIntegerStr[st] + messageIntegerStr[st+1]
-        if int(num) >=97 and int(num) <= 122:
-            st += 2
-        else:
-            num += messageIntegerStr[st + 2]
-            st += 3
-        stringArray.append(chr(int(num)))
-    return ''.join(stringArray)
+from PIL import Image
+import numpy as np
+
+def read_image(path):
+    '''
+        Reads image from file and converts it into numpy array in greyscale
+    '''
+    img = Image.open(path).convert('L') 
+    img_array = np.asarray(img)
+    return img_array.flatten(), img_array.shape
+
